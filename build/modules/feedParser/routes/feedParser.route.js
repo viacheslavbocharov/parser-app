9b2c7e5534ba95d8044dd96eaafcfd89ae7faa2a
viacheslavbocharov <viacheslavbocharov@gmail.com>
2025-09-18 12:35:03 +0100

    chore: add build check to CI

--- build/modules/feedParser/routes/feedParser.route.js
+++ build/modules/feedParser/routes/feedParser.route.js
@@ -1,6 +1,4 @@
 "use strict";
-// import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
-// import { schema } from "../schemas/getFeedData.schema";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getFeedDataRoutes = getFeedDataRoutes;
 const getFeedData_schema_1 = require("../schemas/getFeedData.schema");
--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,5 @@
-// import Fastify, {FastifyServerOptions} from "fastify";
-// import {join} from "node:path";
-// import AutoLoad from "@fastify/autoload";
-// import configPlugin from "./config";
-
-// export type AppOptions = Partial<FastifyServerOptions>
-
-// async function buildApp(options: AppOptions = {}){
-
-//   const fastify = Fastify()
-//     await  fastify.register(configPlugin)
-
-//     try {
-//         fastify.decorate("pluginLoaded", (pluginName: string) => {
-//             fastify.log.info(`✅ Plugin loaded: ${pluginName}`);
-//         });
-
-//         fastify.log.info("Starting to load plugins");
-//         await fastify.register(AutoLoad, {
-//             dir: join(__dirname, "plugins"),
-//             options: options,
-//             ignorePattern: /^((?!plugin).)*$/,
-//         });
-
-//         fastify.log.info("✅ Plugins loaded successfully");
-//     } catch (error) {
-//         fastify.log.error("Error in autoload:", error);
-//         throw error;
-//     }
-
-//     fastify.get("/", async (request, reply) => {
-//         return {hello: "world"}
-//     })
-
-//     return fastify
-// }
-
 import { join } from "node:path";
 import AutoLoad from "@fastify/autoload";
-// export default buildApp
 import Fastify, { type FastifyServerOptions } from "fastify";
 import configPlugin from "./config";
 import { getFeedDataRoutes } from "./modules/feedParser/routes/feedParser.route";
